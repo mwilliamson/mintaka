c5a63a275116af13116a294024c5dabf523c98df
Michael Williamson <mike@zwobble.org>
2024-05-06 15:04:33 +0100

    Support running (but not displaying) multiple processes

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,25 @@
-use std::{io::Read, sync::Arc};
+use std::time::Duration;
 
 use termwiz::terminal::Terminal;
 use wezterm_term::CellAttributes;
 
+use crate::processes::Processes;
+
 mod cli;
 mod config;
+mod processes;
 
 fn main() {
     let config = cli::load_config().unwrap();
-    let command = &config.processes[0].command;
 
-    let pty_system = portable_pty::native_pty_system();
-
-    let pty_pair = pty_system.openpty(portable_pty::PtySize {
-        rows: 24,
-        cols: 80,
-        pixel_width: 0,
-        pixel_height: 0,
-    }).unwrap();
-
-    let mut pty_command = portable_pty::CommandBuilder::new(&command[0]);
-    pty_command.args(command.iter().skip(1));
-    pty_command.cwd(std::env::current_dir().unwrap());
-    let mut child_process = pty_pair.slave.spawn_command(pty_command).unwrap();
-    std::mem::drop(pty_pair.slave);
-
-    let child_process_writer = pty_pair.master.take_writer().unwrap();
-
-    let terminal_size = wezterm_term::TerminalSize::default();
-    let terminal_config = Arc::new(MintakaTerminal);
-    let mut terminal = wezterm_term::Terminal::new(
-        terminal_size,
-        terminal_config,
-        "Mintaka",
-        "1.0.0",
-        Box::new(child_process_writer),
-    );
-
-    let (stdout_tx, stdout_rx) = std::sync::mpsc::channel();
-    let mut child_process_reader = pty_pair.master.try_clone_reader().unwrap();
-    std::thread::spawn(move || {
-        loop {
-            let mut bytes = vec![0; 256];
-            let bytes_read = child_process_reader.read(&mut bytes).unwrap();
-            if bytes_read == 0 {
-                break;
-            }
-            stdout_tx.send(bytes).unwrap();
-        }
-    });
-
-    let return_code = child_process.wait().unwrap();
-    eprintln!("{return_code}");
-    std::mem::drop(pty_pair.master);
-
-    while let Ok(bytes) = stdout_rx.recv() {
-        terminal.advance_bytes(bytes);
+    let mut processes = Processes::new();
+    for process_config in config.processes {
+        processes.start_process(process_config).unwrap();
     }
 
-    let lines = terminal.screen_mut().lines_in_phys_range(0..100);
+    std::thread::sleep(Duration::from_secs(1));
+
+    let lines = processes.lines();
     let real_terminal_capabilities = termwiz::caps::Capabilities::new_from_env().unwrap();
     let mut real_terminal = termwiz::terminal::new_terminal(real_terminal_capabilities).unwrap();
     for line in lines {
@@ -68,14 +29,5 @@ fn main() {
             termwiz::surface::Change::Text("\r\n".to_owned()),
             termwiz::surface::Change::AllAttributes(CellAttributes::blank()),
         ]).unwrap();
-    }
-}
-
-#[derive(Debug)]
-struct MintakaTerminal;
-
-impl wezterm_term::TerminalConfiguration for MintakaTerminal {
-    fn color_palette(&self) -> wezterm_term::color::ColorPalette {
-        wezterm_term::color::ColorPalette::default()
     }
 }
---
+++ src/processes.rs
@@ -0,0 +1,124 @@
+use std::sync::{Arc, Mutex};
+
+use portable_pty::PtySystem;
+
+use crate::config::ProcessConfig;
+
+pub(crate) struct Processes {
+    pty_system: Box<dyn PtySystem>,
+
+    pty_size: portable_pty::PtySize,
+
+    processes: Vec<Process>,
+}
+
+impl Processes {
+    pub(crate) fn new() -> Self {
+        let pty_system = portable_pty::native_pty_system();
+        let pty_size = portable_pty::PtySize {
+            rows: 24,
+            cols: 80,
+            pixel_width: 0,
+            pixel_height: 0,
+        };
+
+        Self { pty_system, pty_size, processes: Vec::new() }
+    }
+
+    pub(crate) fn start_process(
+        &mut self,
+        process_config: ProcessConfig,
+    ) -> Result<(), ProcessError> {
+        let pty_command = Self::process_config_to_pty_command(process_config)?;
+
+        let pty_pair = self.pty_system.openpty(self.pty_size).unwrap();
+
+        let child_process = pty_pair.slave.spawn_command(pty_command).unwrap();
+        std::mem::drop(pty_pair.slave);
+
+        let child_process_writer = pty_pair.master.take_writer().unwrap();
+        let terminal = Arc::new(Mutex::new(Self::create_process_terminal(child_process_writer)));
+
+        let child_process_reader = pty_pair.master.try_clone_reader().unwrap();
+        Self::spawn_process_reader(child_process_reader, Arc::clone(&terminal));
+
+        let process = Process {
+            _child_process: child_process,
+            terminal,
+            _pty_master: pty_pair.master,
+        };
+        self.processes.push(process);
+
+        Ok(())
+    }
+
+    fn process_config_to_pty_command(process_config: ProcessConfig) -> Result<portable_pty::CommandBuilder, ProcessError> {
+        let executable = process_config.command.first()
+            .ok_or(ProcessError::ProcessConfigMissingCommand)?;
+        let mut pty_command = portable_pty::CommandBuilder::new(executable);
+
+        pty_command.args(process_config.command.iter().skip(1));
+
+        let current_dir = std::env::current_dir().map_err(ProcessError::GetCurrentDirFailed)?;
+        pty_command.cwd(current_dir);
+
+        Ok(pty_command)
+    }
+
+    fn create_process_terminal(writer: Box<dyn std::io::Write + Send>) -> wezterm_term::Terminal {
+        let terminal_size = wezterm_term::TerminalSize::default();
+        let terminal_config = Arc::new(ProcessTerminal);
+        wezterm_term::Terminal::new(
+            terminal_size,
+            terminal_config,
+            "Mintaka",
+            "1.0.0",
+            Box::new(writer),
+        )
+    }
+
+    fn spawn_process_reader(
+        mut reader: Box<dyn std::io::Read + Send>,
+        terminal: Arc<Mutex<wezterm_term::Terminal>>,
+    ) {
+        std::thread::spawn(move || {
+            let mut bytes = vec![0; 256];
+            loop {
+                let bytes_read = reader.read(&mut bytes).unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                terminal.lock().unwrap().advance_bytes(&bytes[..bytes_read]);
+            }
+        });
+    }
+
+    pub(crate) fn lines(&self) -> Vec<wezterm_term::Line> {
+        let terminal = self.processes[0].terminal.lock().unwrap();
+        terminal.screen().lines_in_phys_range(0..100)
+    }
+}
+
+struct Process {
+    _child_process: Box<dyn portable_pty::Child>,
+    terminal: Arc<Mutex<wezterm_term::Terminal>>,
+    _pty_master: Box<dyn portable_pty::MasterPty>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) enum ProcessError {
+    ProcessConfigMissingCommand,
+
+    GetCurrentDirFailed(std::io::Error),
+}
+
+
+#[derive(Debug)]
+struct ProcessTerminal;
+
+impl wezterm_term::TerminalConfiguration for ProcessTerminal {
+    fn color_palette(&self) -> wezterm_term::color::ColorPalette {
+        wezterm_term::color::ColorPalette::default()
+    }
+}
