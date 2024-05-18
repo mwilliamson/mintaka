cae863c7ddd16c84de6cb95a81792fecbb7e781f
Michael Williamson <mike@zwobble.org>
2024-05-18 16:38:14 +0100

    Extract process start logic to Process

--- src/processes.rs
+++ src/processes.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use portable_pty::PtySystem;
+use portable_pty::{PtyPair, PtySystem};
 use termwiz::terminal::TerminalWaker;
 use wezterm_term::{TerminalSize, VisibleRowIndex};
 
@@ -41,91 +41,13 @@ impl Processes {
         &mut self,
         process_config: ProcessConfig,
     ) -> Result<(), ProcessError> {
-        let pty_command = Self::process_config_to_pty_command(&process_config)?;
-
         let pty_pair = self.pty_system.openpty(self.pty_size).unwrap();
 
-        let child_process = pty_pair.slave.spawn_command(pty_command).unwrap();
-        std::mem::drop(pty_pair.slave);
+        let process = Process::start(process_config, pty_pair, self.on_change.clone())?;
 
-        let child_process_writer = pty_pair.master.take_writer().unwrap();
-        let terminal = Arc::new(Mutex::new(Self::create_process_terminal(child_process_writer)));
-
-        let process_status = Arc::new(Mutex::new(ProcessStatus::Ok));
-
-        let child_process_reader = pty_pair.master.try_clone_reader().unwrap();
-        self.spawn_process_reader(
-            process_config.process_type(),
-            child_process_reader,
-            Arc::clone(&process_status),
-            Arc::clone(&terminal),
-        );
-
-        let process = Process {
-            name: process_config.name.unwrap_or_else(|| process_config.command.join(" ")),
-            status: process_status,
-            _child_process: child_process,
-            terminal,
-            pty_master: pty_pair.master,
-        };
         self.processes.push(process);
 
         Ok(())
-    }
-
-    fn process_config_to_pty_command(process_config: &ProcessConfig) -> Result<portable_pty::CommandBuilder, ProcessError> {
-        let executable = process_config.command.first()
-            .ok_or(ProcessError::ProcessConfigMissingCommand)?;
-        let mut pty_command = portable_pty::CommandBuilder::new(executable);
-
-        pty_command.args(process_config.command.iter().skip(1));
-
-        let current_dir = std::env::current_dir().map_err(ProcessError::GetCurrentDirFailed)?;
-        pty_command.cwd(current_dir);
-
-        Ok(pty_command)
-    }
-
-    fn create_process_terminal(writer: Box<dyn std::io::Write + Send>) -> wezterm_term::Terminal {
-        let terminal_size = wezterm_term::TerminalSize::default();
-        let terminal_config = Arc::new(ProcessTerminal);
-        wezterm_term::Terminal::new(
-            terminal_size,
-            terminal_config,
-            "Mintaka",
-            "1.0.0",
-            Box::new(writer),
-        )
-    }
-
-    fn spawn_process_reader(
-        &self,
-        process_type: ProcessType,
-        mut reader: Box<dyn std::io::Read + Send>,
-        process_status: Arc<Mutex<ProcessStatus>>,
-        terminal: Arc<Mutex<wezterm_term::Terminal>>,
-    ) {
-        let on_change = self.on_change.clone();
-        std::thread::spawn(move || {
-            let mut bytes = vec![0; 256];
-            loop {
-                let bytes_read = reader.read(&mut bytes).unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
-                let mut terminal_locked = terminal.lock().unwrap();
-                terminal_locked.advance_bytes(&bytes[..bytes_read]);
-
-                let screen = terminal_locked.screen();
-
-                if let Some(new_status) = process_types::status(&process_type, screen) {
-                    let mut process_status_locked = process_status.lock().unwrap();
-                    *process_status_locked = new_status;
-                }
-
-                on_change.wake().unwrap();
-            }
-        });
     }
 
     pub(crate) fn processes(&self) -> &[Process] {
@@ -188,6 +110,93 @@ pub(crate) struct Process {
 }
 
 impl Process {
+    fn start(
+        process_config: ProcessConfig,
+        pty_pair: PtyPair,
+        on_change: TerminalWaker,
+    ) -> Result<Self, ProcessError> {
+        let pty_command = Self::process_config_to_pty_command(&process_config)?;
+
+        let child_process = pty_pair.slave.spawn_command(pty_command).unwrap();
+        std::mem::drop(pty_pair.slave);
+
+        let child_process_writer = pty_pair.master.take_writer().unwrap();
+        let terminal = Arc::new(Mutex::new(Self::create_process_terminal(child_process_writer)));
+
+        let process_status = Arc::new(Mutex::new(ProcessStatus::Ok));
+
+        let child_process_reader = pty_pair.master.try_clone_reader().unwrap();
+        Self::spawn_process_reader(
+            process_config.process_type(),
+            child_process_reader,
+            Arc::clone(&process_status),
+            Arc::clone(&terminal),
+            on_change,
+        );
+
+        Ok(Process {
+            name: process_config.name.unwrap_or_else(|| process_config.command.join(" ")),
+            status: process_status,
+            _child_process: child_process,
+            terminal,
+            pty_master: pty_pair.master,
+        })
+    }
+
+    fn process_config_to_pty_command(process_config: &ProcessConfig) -> Result<portable_pty::CommandBuilder, ProcessError> {
+        let executable = process_config.command.first()
+            .ok_or(ProcessError::ProcessConfigMissingCommand)?;
+        let mut pty_command = portable_pty::CommandBuilder::new(executable);
+
+        pty_command.args(process_config.command.iter().skip(1));
+
+        let current_dir = std::env::current_dir().map_err(ProcessError::GetCurrentDirFailed)?;
+        pty_command.cwd(current_dir);
+
+        Ok(pty_command)
+    }
+
+    fn create_process_terminal(writer: Box<dyn std::io::Write + Send>) -> wezterm_term::Terminal {
+        let terminal_size = wezterm_term::TerminalSize::default();
+        let terminal_config = Arc::new(ProcessTerminal);
+        wezterm_term::Terminal::new(
+            terminal_size,
+            terminal_config,
+            "Mintaka",
+            "1.0.0",
+            Box::new(writer),
+        )
+    }
+
+    fn spawn_process_reader(
+        process_type: ProcessType,
+        mut reader: Box<dyn std::io::Read + Send>,
+        process_status: Arc<Mutex<ProcessStatus>>,
+        terminal: Arc<Mutex<wezterm_term::Terminal>>,
+        on_change: TerminalWaker,
+    ) {
+        std::thread::spawn(move || {
+            let mut bytes = vec![0; 256];
+            loop {
+                let bytes_read = reader.read(&mut bytes).unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                let mut terminal_locked = terminal.lock().unwrap();
+                terminal_locked.advance_bytes(&bytes[..bytes_read]);
+
+                let screen = terminal_locked.screen();
+
+                if let Some(new_status) = process_types::status(&process_type, screen) {
+                    let mut process_status_locked = process_status.lock().unwrap();
+                    *process_status_locked = new_status;
+                }
+
+                on_change.wake().unwrap();
+            }
+        });
+    }
+
     pub(crate) fn status(&self) -> ProcessStatus {
         *self.status.lock().unwrap()
     }
